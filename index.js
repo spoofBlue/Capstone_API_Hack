aa8f347dc5eaf18557a50a6cff80e26dd83abadc
Cory Nanni <cory.nanni@gmail.com>
2018-04-11 18:12:51 -0400

    fading adjustment

--- index.js
+++ index.js
@@ -499,7 +499,7 @@ function main() {
   
   // Changes the color saturation of section using CSS.
   function addFadedClass(section) {
-    $(section).fadeTo("slow", 0.25);
+    $(section).fadeTo("slow", 0.3);
   }
   
   function removeFadedClass(section) {

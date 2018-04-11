c329aa25da9dc9cb8b0ceb39efbf278cc04bc1e3
Cory Nanni <cory.nanni@gmail.com>
2018-04-11 17:18:13 -0400

    minor exitSection edit

--- index.js
+++ index.js
@@ -337,7 +337,7 @@ function main() {
   
   //  Change which section user returns to, based on current location when requesting exit.
   function exitSection() {
-    if (TEXTBOX_MODE === true) {
+    if (TEXTBOX_MODE === true && INFO_MODE === true) {
         startTextboxMode();
     } else {
         startMapMode();
